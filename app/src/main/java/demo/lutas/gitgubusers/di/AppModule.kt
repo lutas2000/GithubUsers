89dfed95f6af5c3fadaaa6a71bfb946575b5fafc
lutas2000 <wsrew2000@gmail.com>
2021-07-04 00:55:59 +0800

    feat: add UserListViewModel into AppModule

--- app/src/main/java/demo/lutas/gitgubusers/di/AppModule.kt
+++ app/src/main/java/demo/lutas/gitgubusers/di/AppModule.kt
@@ -2,9 +2,14 @@ package demo.lutas.gitgubusers.di
 
 import demo.lutas.gitgubusers.domain.data.repositories.UserRepository
 import demo.lutas.gitgubusers.presentation.data.repositories.UserRemoteRepository
+import demo.lutas.gitgubusers.presentation.viewmodels.UserListViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
     // Repository
     single<UserRepository> { UserRemoteRepository(get()) }
+
+    // ViewModel
+    viewModel { UserListViewModel(get()) }
 }
