54faeabcda2d0e1bcf8a8163eeb0ec7263e008c4
lutas2000 <wsrew2000@gmail.com>
2021-07-03 15:55:19 +0800

    fix: remove `accept` param from UserRepository.getUsers

--- app/src/main/java/demo/lutas/gitgubusers/domain/data/repositories/UserRepository.kt
+++ app/src/main/java/demo/lutas/gitgubusers/domain/data/repositories/UserRepository.kt
@@ -3,6 +3,6 @@ package demo.lutas.gitgubusers.domain.data.repositories
 import demo.lutas.gitgubusers.domain.data.entities.User
 
 interface UserRepository {
-    suspend fun getUsers(accept: String, since: Int, per_page: Int): List<User>
+    suspend fun getUsers(since: Int, per_page: Int): List<User>
     suspend fun getUserDetail(): User
 }
