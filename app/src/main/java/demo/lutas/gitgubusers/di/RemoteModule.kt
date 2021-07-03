00ffa1106fd7927312daa4dea39fc17e374da771
lutas2000 <wsrew2000@gmail.com>
2021-07-03 16:00:31 +0800

    feat: setup UserService into RemoteModule

--- app/src/main/java/demo/lutas/gitgubusers/di/RemoteModule.kt
+++ app/src/main/java/demo/lutas/gitgubusers/di/RemoteModule.kt
@@ -1,10 +1,12 @@
 package demo.lutas.gitgubusers.di
 
 import demo.lutas.gitgubusers.presentation.data.remote.RestClient
+import demo.lutas.gitgubusers.presentation.data.remote.UserService
 import org.koin.dsl.module
 
 val remoteModule = module {
     single { RestClient() }
+    single { createService<UserService>(get()) }
 }
 
 private inline fun <reified T> createService(client: RestClient): T {
