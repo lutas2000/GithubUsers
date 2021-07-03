f9368bd3eb9f9d93db24fefe05375ef7f9704318
lutas2000 <wsrew2000@gmail.com>
2021-07-03 15:51:39 +0800

    feat: create UserService

---
+++ app/src/main/java/demo/lutas/gitgubusers/presentation/data/remote/UserService.kt
@@ -0,0 +1,13 @@
+package demo.lutas.gitgubusers.presentation.data.remote
+
+import demo.lutas.gitgubusers.domain.data.entities.User
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface UserService {
+    @GET("/users")
+    suspend fun getUsers(
+        @Query("since") since: Int,
+        @Query("per_page") skip: Int,
+    ): List<User>
+}
