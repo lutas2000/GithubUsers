960738304726aac353e06a5cd4aa3d9e2a8d674e
lutas2000 <wsrew2000@gmail.com>
2021-07-04 16:08:31 +0800

    feat: UserListViewModel - replace LiveData with Pager Flow

--- app/src/main/java/demo/lutas/gitgubusers/presentation/viewmodels/UserListViewModel.kt
+++ app/src/main/java/demo/lutas/gitgubusers/presentation/viewmodels/UserListViewModel.kt
@@ -1,26 +1,20 @@
 package demo.lutas.gitgubusers.presentation.viewmodels
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import demo.lutas.gitgubusers.domain.data.repositories.UserRepository
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.onStart
-import kotlinx.coroutines.launch
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.cachedIn
+import demo.lutas.gitgubusers.presentation.data.remote.UserService
+import demo.lutas.gitgubusers.presentation.data.repositories.UserListPagingSource
 
-class UserListViewModel(private val userRepository: UserRepository): ViewModel() {
-    private val _state = MutableLiveData<UserListState>()
-    val state: LiveData<UserListState> get() = _state
+class UserListViewModel(private val userService: UserService): ViewModel() {
     private val perPage = 20
 
-    fun getUserList() {
-        viewModelScope.launch {
-            userRepository.getUsers(0, perPage)
-                .onStart { _state.value = UserListState.Loading }
-                .catch { e -> _state.value = UserListState.Fail(e) }
-                .collect { value -> _state.value = UserListState.Success(value) }
-        }
-    }
+    val flow = Pager(
+        PagingConfig(pageSize = perPage)
+    ) {
+        UserListPagingSource(userService, perPage)
+    }.flow
+        .cachedIn(viewModelScope)
 }
