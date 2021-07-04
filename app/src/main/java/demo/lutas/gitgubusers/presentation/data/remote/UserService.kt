1ff4997fcb87eb7fd3d1551528ae5752a869e0ad
lutas2000 <wsrew2000@gmail.com>
2021-07-04 16:18:29 +0800

    feat: UserService - add getUserDetail function

--- app/src/main/java/demo/lutas/gitgubusers/presentation/data/remote/UserService.kt
+++ app/src/main/java/demo/lutas/gitgubusers/presentation/data/remote/UserService.kt
@@ -2,6 +2,7 @@ package demo.lutas.gitgubusers.presentation.data.remote
 
 import demo.lutas.gitgubusers.domain.data.entities.User
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface UserService {
@@ -10,4 +11,9 @@ interface UserService {
         @Query("since") since: Int,
         @Query("per_page") perPage: Int,
     ): List<User>
+
+    @GET("/users/{userName}")
+    suspend fun getUserDetail(
+        @Path("userName") userName: String
+    ): User
 }
