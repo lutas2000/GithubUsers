89bcc2c95026d569e0470982da2786612f68a079
lutas2000 <wsrew2000@gmail.com>
2021-07-04 22:24:51 +0800

    feat: setup UserDetailViewModel in AppModule

--- app/src/main/java/demo/lutas/gitgubusers/di/AppModule.kt
+++ app/src/main/java/demo/lutas/gitgubusers/di/AppModule.kt
@@ -2,6 +2,7 @@ package demo.lutas.gitgubusers.di
 
 import demo.lutas.gitgubusers.domain.data.repositories.UserRepository
 import demo.lutas.gitgubusers.presentation.data.repositories.UserRemoteRepository
+import demo.lutas.gitgubusers.presentation.viewmodels.UserDetailViewModel
 import demo.lutas.gitgubusers.presentation.viewmodels.UserListViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -12,4 +13,5 @@ val appModule = module {
 
     // ViewModel
     viewModel { UserListViewModel(get()) }
+    viewModel { UserDetailViewModel(get()) }
 }
