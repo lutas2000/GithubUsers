115c6f69e9cad76dc2e6ef10e0ccdaef03af87ca
lutas2000 <wsrew2000@gmail.com>
2021-07-04 16:09:33 +0800

    feat: lunch UserListFragment on MainActivity created

--- app/src/main/java/demo/lutas/gitgubusers/presentation/views/MainActivity.kt
+++ app/src/main/java/demo/lutas/gitgubusers/presentation/views/MainActivity.kt
@@ -3,10 +3,16 @@ package demo.lutas.gitgubusers.presentation.views
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import demo.lutas.gitgubusers.R
+import demo.lutas.gitgubusers.presentation.views.user_list.UserListFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.main_fragment, UserListFragment())
+        fragmentTransaction.commit()
     }
 }
